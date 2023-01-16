4e0d9269e56c1f5500ad002f918f532aac992f05
pengyuhe <pyhejhu@gmail.com>
2023-01-16 15:00:31 -0500

    basic atomic bool operations

---
+++ chapter5/basic_atomic_bool.cpp
@@ -0,0 +1,34 @@
+#include <atomic>
+#include <iostream>
+
+int main(){
+
+	// Basic value set.
+	std::atomic<bool> b(false);
+	bool x = b.load(std::memory_order_acquire);
+	b.store(true);
+	x = b.exchange(false,std::memory_order_acq_rel);
+	// x equals b befoer exchange, which is true;
+	// b is exchanged to false.
+	std::cout<<"x= "<<x<< " b= "<<b.load()<<std::endl;
+	std::cout<<std::endl;
+
+	// compare and exchange
+	b.store(true);
+	bool expected = false;
+	while(1){
+		// case 1: b = true, expected=false
+		//  compare_exchaneg_weak return false. Finish in 1 loop
+		//  b=true, expected = true (set b.load() to expected)
+		// case 2: b = false, expected = false
+		// 
+		bool result = (!b.compare_exchange_weak(expected,true) && !expected);
+		std::cout<<"b = "<<b.load()<<" expected= "<<expected<<" result= "<<result<<std::endl;
+		if(!result){
+			break;
+		}
+	}
+
+	std::cout<<"After loop b = "<<b.load()<<" expected= "<<expected<<std::endl;
+
+}
